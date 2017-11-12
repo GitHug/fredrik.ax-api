c4a176d598180877d91cbb2a6ce65ef0ddc6c18c
Fredrik Mäkilä <fredrik.makila@gmail.com>
2017-11-12 20:03:54 +0000

    Add some middleware

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
 var routes = require('./api/routes/appRoutes'); //importing route
 routes(app); //register the route
 
