0f7fa6c48cd7e12ae0f5d36ca107f5fee8bb43ec
Fredrik Mäkilä <fredrik.makila@gmail.com>
2017-11-15 21:31:49 +0000

    Try to get CORS to work

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ bodyParser = require('body-parser');
 const path = require('path');
 const expressValidator = require('express-validator');
 
+if (req.method === 'OPTIONS') {
+    console.log('!OPTIONS');
+    var headers = {};
+    // IE8 does not allow domains to be specified, just the *
+    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+    headers["Access-Control-Allow-Origin"] = "*";
+    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+    headers["Access-Control-Allow-Credentials"] = false;
+    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+    res.writeHead(200, headers);
+    res.end();
+}
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
