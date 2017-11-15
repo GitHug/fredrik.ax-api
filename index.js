3e77492a285e08957f1a2f71bc756ed4538e8556
Fredrik Mäkilä <fredrik.makila@gmail.com>
2017-11-15 21:35:23 +0000

    Total fail last time, one more try

--- index.js
+++ index.js
@@ -5,19 +5,12 @@ bodyParser = require('body-parser');
 const path = require('path');
 const expressValidator = require('express-validator');
 
-if (req.method === 'OPTIONS') {
-    console.log('!OPTIONS');
-    var headers = {};
-    // IE8 does not allow domains to be specified, just the *
-    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-    headers["Access-Control-Allow-Origin"] = "*";
-    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-    headers["Access-Control-Allow-Credentials"] = false;
-    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-    res.writeHead(200, headers);
-    res.end();
-}
+app.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  });
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
